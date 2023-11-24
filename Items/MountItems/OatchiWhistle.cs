047eb53beabecca769d7b41cc5aa104b17889bb3
Moo Moo <obsidian1392@gmail.com>
2023-11-23 22:21:13 -0700

    Removed old worldgen item code

--- Items/MountItems/OatchiWhistle.cs
+++ Items/MountItems/OatchiWhistle.cs
@@ -27,46 +27,5 @@ namespace OneBlock.Items.MountItems
             Item.noMelee = true;
             //Item.mountType = ModContent.MountType<Oatchi>();
         }
-
-        public override bool? UseItem(Player player)
-        {
-            Main.dungeonX = Main.maxTilesX / 20;
-
-            for (int i = 0; i < Main.maxTilesX; i++)
-            {
-                for (int j = 0; j < Main.maxTilesY; j++)
-                {
-                    Tile tile = Main.tile[i, j];
-                    if (tile == null) continue;
-
-                    tile.ClearTile();
-                    tile.WallType = WallID.None;
-                    tile.LiquidAmount = 0;
-                }
-            }
-
-            Main.spawnTileX = Main.maxTilesX / 2;
-            Main.spawnTileY = Main.maxTilesY / 2 - Main.maxTilesY / 3; // Re-adjusting the spawn point to a constant value.
-
-            if (Main.dungeonX < Main.maxTilesX / 2)
-            {
-                dungeonSide = 0;
-            }
-            else
-            {
-                dungeonSide = 1;
-            }
-
-            GenStartingPlatform();
-            GenDungeonPlatform();
-            GenUnderworldIslands();
-            GenSnowIslands();
-            GenChlorophytePlanetoids();
-            GenHivePlanetoids();
-            GenForestPlanetoids();
-            GenSnowPlanetoids();
-            GenEvilPlanetoids();
-            return true;
-        }
     }
 }

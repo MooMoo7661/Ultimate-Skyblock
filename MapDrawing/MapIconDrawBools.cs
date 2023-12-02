bb09744adea617342f81061b432c116f52c6c16e
Moo Moo <obsidian1392@gmail.com>
2023-12-02 14:12:18 -0700

    Added

---
+++ MapDrawing/MapIconDrawBools.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneBlock.MapDrawing
+{
+    public class MapIconDrawBools
+    {
+        public static bool MapIconDungeon = true;
+        public static bool MapIconEvil = true;
+        public static bool MapIconForest = true;
+        public static bool MapIconJungle = true;
+        public static bool MapIconSnow = true;
+        public static bool Hell = true;
+        public static bool Mushroom = true;
+
+        public static bool AllIcons = true;
+    }
+}

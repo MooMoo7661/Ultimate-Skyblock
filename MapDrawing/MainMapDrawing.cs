de9c4b59188d68cddf24fd654fb2d2b5a098fe95
Moo Moo <obsidian1392@gmail.com>
2023-12-02 14:13:09 -0700

    Added configs

--- MapDrawing/MainMapDrawing.cs
+++ MapDrawing/MainMapDrawing.cs
@@ -22,9 +22,7 @@ namespace OneBlock.MapDrawing
         public static string path = "OneBlock/MapDrawing/Icons/";
         public override void Draw(ref MapOverlayDrawContext context, ref string text)
         {
-            var config = ModContent.GetInstance<MapIconDrawConfig>();
-
-            if (!config.DrawMapIcons) { return; }
+            if (!MapIconDrawBools.AllIcons) { return; }
 
             const float scaleIfNotSelected = 1f;
             const float scaleIfSelected = 1.5f;
@@ -41,43 +39,43 @@ namespace OneBlock.MapDrawing
 
             string evilText = WorldGen.crimson ? "Crimson" : "Corruption";
 
-            if (config.MapIconDungeon)
+            if (MapIconDrawBools.MapIconDungeon)
             {
                 var dungeon = context.Draw(dungeonIcon, new Vector2(Main.dungeonX, Main.dungeonY), Color.White, new SpriteFrame(1, 1, 0, 0), scaleIfNotSelected, scaleIfSelected, Alignment.Center);
                 if (dungeon.IsMouseOver) { text = "Dungeon"; }
             }
 
-            if (config.MapIconEvil)
+            if (MapIconDrawBools.MapIconEvil)
             {
                 var evil = context.Draw(corruptionIcon, new Vector2(WorldHelpers.Evil.X, universalY), Color.White, new SpriteFrame(1, 1, 0, 0), scaleIfNotSelected, scaleIfSelected, Alignment.Center);
                 if (evil.IsMouseOver) { text = evilText; }
             }
 
-            if (config.MapIconForest)
+            if (MapIconDrawBools.MapIconForest)
             {
                 var forest = context.Draw(forestIcon, new Vector2(Main.spawnTileX, universalY), Color.White, new SpriteFrame(1, 1, 0, 0), scaleIfNotSelected, scaleIfSelected, Alignment.Center);
                 if (forest.IsMouseOver) { text = "Forest"; }
             }
 
-            if (config.MapIconJungle)
+            if (MapIconDrawBools.MapIconJungle)
             {
                 var jungle = context.Draw(jungleIcon, new Vector2(WorldHelpers.Jungle.X + 400, universalY), Color.White, new SpriteFrame(1, 1, 0, 0), scaleIfNotSelected, scaleIfSelected, Alignment.Center);
                 if (jungle.IsMouseOver) { text = "Jungle"; }
             }
 
-            if (config.MapIconSnow)
+            if (MapIconDrawBools.MapIconSnow)
             {
                 var snow = context.Draw(snowIcon, new Vector2(WorldHelpers.Snow.X + 400 + MainWorld.ScaleBasedOnWorldSizeX * 1.5f, universalY), Color.White, new SpriteFrame(1, 1, 0, 0), scaleIfNotSelected, scaleIfSelected, Alignment.Center);
                 if (snow.IsMouseOver) { text = "Snow"; }
             }
 
-            if (config.Hell)
+            if (MapIconDrawBools.Hell)
             {
                 var hell = context.Draw(hellIcon, new Vector2(WorldHelpers.Hell.X, WorldHelpers.Hell.Y), Color.White, new SpriteFrame(1, 1, 0, 0), scaleIfNotSelected, scaleIfSelected, Alignment.Center);
                 if (hell.IsMouseOver) { text = "Hell"; }
             }
 
-            if (config.Mushroom)
+            if (MapIconDrawBools.Mushroom)
             {
                 var mushroom = context.Draw(mushroomIcon,new(WorldHelpers.Mushroom.X + 50, WorldHelpers.Mushroom.Y + 30), Color.White, new SpriteFrame(1, 1, 0, 0), scaleIfNotSelected, scaleIfSelected, Alignment.Center);
                 if (mushroom.IsMouseOver) { text = "Mushroom"; }

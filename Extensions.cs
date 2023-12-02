342e193a7f8817938853bbbe6bfdc29b53248b15
Moo Moo <obsidian1392@gmail.com>
2023-12-02 14:09:30 -0700

    🔥 Added extensions 🔥

---
+++ Extensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace OneBlock
+{
+    public static class Extensions
+    {
+        public static int ToSeconds(this int seconds) => seconds * 60;
+        public static float ToSeconds(this float seconds) => seconds * 60;
+
+        public static Vector2 ToVector2(this Tile tile, int x, int y) => new Vector2(x, y);
+    }
+}

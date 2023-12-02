321366e9b9eb9bd8a7c3406c7dc37953eb1f88e8
Moo Moo <obsidian1392@gmail.com>
2023-12-02 14:13:33 -0700

    Moved configs + added notifs config

---
+++ Configs/NotificationsConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader.Config;
+
+namespace OneBlock.Configs
+{
+    public class NotificationsConfig : ModConfig
+    {
+        public override ConfigScope Mode => ConfigScope.ClientSide;
+
+        [Label("All Configs Enabled")]
+        [DefaultValue(true)]
+        public bool EnabledNotifs { get; set; }
+    }
+}

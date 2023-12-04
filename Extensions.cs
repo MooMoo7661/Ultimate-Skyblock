05dd0c19c244c47465ded0fa23772585f8b9b961
Moo Moo <obsidian1392@gmail.com>
2023-12-03 17:50:06 -0700

    Attempt at simplifying chest adding (WIP)

--- Extensions.cs
+++ Extensions.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.Xna.Framework;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Terraria;
+using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace OneBlock
@@ -13,7 +15,17 @@ namespace OneBlock
     {
         public static int ToSeconds(this int seconds) => seconds * 60;
         public static float ToSeconds(this float seconds) => seconds * 60;
-
         public static Vector2 ToVector2(this Tile tile, int x, int y) => new Vector2(x, y);
+        public static void Add(this Chest chest, Item item)
+        {
+            for (int i = 0; i < chest.item.Length; i++)
+            {
+                if (chest.item[i].IsAir)
+                {
+                    chest.item[i] = item;
+                    return;
+                }
+            }
+        }
     }
 }

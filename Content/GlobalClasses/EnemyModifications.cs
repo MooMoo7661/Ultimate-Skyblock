c805141bbdb9ae7730231aae577e871cde1d6772
Moo Moo <obsidian1392@gmail.com>
2024-02-18 16:26:27 -0700

    Added pools for deepstone biome

--- Content/GlobalClasses/EnemyModifications.cs
+++ Content/GlobalClasses/EnemyModifications.cs
@@ -1,6 +1,11 @@
+using System.Collections.Generic;
+using SubworldLibrary;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
+using UltimateSkyblock.Content.Biomes;
+using UltimateSkyblock.Content.NPCs;
+using UltimateSkyblock.Content.Subworlds;
 
 namespace UltimateSkyblock.Content.GlobalClasses
 {
@@ -21,6 +26,31 @@ namespace UltimateSkyblock.Content.GlobalClasses
             {
                 npc.RequestBuffRemoval(BuffID.OnFire);
                 npc.RequestBuffRemoval(BuffID.OnFire3);
+            }
+        }
+
+        public override void EditSpawnPool(IDictionary<int, float> pool, NPCSpawnInfo spawnInfo)
+        {
+            if (SubworldSystem.IsActive<MiningSubworld>())
+            {
+                if (spawnInfo.Player.ZoneNormalCaverns)
+                {
+                    pool.Add(NPCID.WallCreeper, 0.7f);
+                    pool.Add(NPCID.CaveBat, 1f);
+                }
+
+                if (spawnInfo.Player.InModBiome<DeepstoneBiome>())
+                {
+                    pool.Clear();
+                    pool.Add(NPCID.Ghost, 0.5f);
+                    pool.Add(ModContent.NPCType<DeepstoneBat>(), 0.9f);
+                    pool.Add(NPCID.ArmoredSkeleton, 0.3f);
+                    pool.Add(NPCID.WallCreeper, 0.5f);
+                    pool.Add(NPCID.Skeleton, 0.3f);
+                    pool.Add(NPCID.Demon, 0.6f);
+                    pool.Add(NPCID.BlackSlime, 0.8f);
+                    pool.Add(NPCID.Worm, 0.4f);
+                }    
             }
         }
 

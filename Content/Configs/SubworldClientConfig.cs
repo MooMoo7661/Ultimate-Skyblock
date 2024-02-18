129689e275e4130c783e1643be850e8a7e0491cb
Moo Moo <obsidian1392@gmail.com>
2024-02-18 16:26:04 -0700

    Added subworld configs

---
+++ Content/Configs/SubworldClientConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel;
+using Terraria.ModLoader.Config;
+using UltimateSkyblock.Content.Subworlds;
+
+namespace UltimateSkyblock.Content.Configs
+{
+    public class SubworldClientConfig : ModConfig
+    {
+        public override ConfigScope Mode => ConfigScope.ClientSide;
+
+        [Header("WorldGenDebugInfo")]
+        [Label("Show Fast Noise Seed")]
+        [DefaultValue(true)]
+        [BackgroundColor(0, 255, 0)]
+        public bool ShowFastNoiseSeed { get; set; }
+
+        [Label("Show Fractal Type")]
+        [DefaultValue(false)]
+        [BackgroundColor(0, 255, 0)]
+        public bool ShowFractalType { get; set; }
+
+        [Label("Show Perlin Type")]
+        [DefaultValue(false)]
+        [BackgroundColor(0, 255, 0)]
+        public bool ShowPerlinType { get; set; }
+    }
+}
---
+++ Content/Configs/SubworldConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel;
+using Terraria.ModLoader.Config;
+using UltimateSkyblock.Content.Subworlds;
+
+namespace UltimateSkyblock.Content.Configs
+{
+    public class SubworldConfig : ModConfig
+    {
+        public override ConfigScope Mode => ConfigScope.ServerSide;
+
+        [Header("ColorGuide")]
+        [DefaultValue(3)]
+        [BackgroundColor(255, 0, 0)]
+        [Slider]
+        [Range(0, 5)]
+        [ReloadRequired]
+        public int PerlinNoiseType { get; set; }
+
+        [DefaultValue(1)]
+        [BackgroundColor(255, 0, 0)]
+        [Slider]
+        [Range(0, 5)]
+        [ReloadRequired]
+        public int FractalType { get; set; }
+    }
+}

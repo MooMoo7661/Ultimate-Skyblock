88ebdf4129e870ef910b46de323f76b5fbc7b46a
Moo Moo <obsidian1392@gmail.com>
2024-02-18 16:27:12 -0700

    Added deepstone brick, chest & gravel

---
+++ Content/Items/Placeable/DeepstoneChest.cs
@@ -0,0 +1,35 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+using UltimateSkyblock.Content.Tiles.Blocks;
+
+namespace UltimateSkyblock.Content.Items.Placeable
+{
+    public class DeepstoneChest : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.DefaultToPlaceableTile(ModContent.TileType<Tiles.Blocks.DeepstoneChestTile>());
+             Item.placeStyle = 1; // Use this to place the chest in its locked style
+            Item.width = 26;
+            Item.height = 22;
+            Item.value = 500;
+        }
+    }
+
+    public class DeepstoneChestKey : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Item.ResearchUnlockCount = 5; // Biome keys usually take 1 item to research instead.
+        }
+
+        public override void SetDefaults()
+        {
+            Item.CloneDefaults(ItemID.GoldenKey);
+            Item.rare = ItemRarityID.Orange;
+            Item.value = Item.buyPrice(gold: 3, silver: 42);
+            Item.scale = 1.4f;
+        }
+    }
+}

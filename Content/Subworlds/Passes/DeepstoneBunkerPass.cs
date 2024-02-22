c58b0c110254b460535434b1c6f754cd8da03fad
Moo Moo <obsidian1392@gmail.com>
2024-02-22 08:38:29 -0700

    New rooms + partial basement generation

--- Content/Subworlds/Passes/DeepstoneBunkerPass.cs
+++ Content/Subworlds/Passes/DeepstoneBunkerPass.cs
@@ -13,6 +13,7 @@ using Microsoft.Xna.Framework;
 using StructureHelper;
 using Terraria.DataStructures;
 using Terraria.Utilities;
+using Terraria.ModLoader;
 
 namespace UltimateSkyblock.Content.Subworlds.Passes
 {
@@ -23,6 +24,7 @@ namespace UltimateSkyblock.Content.Subworlds.Passes
         public static string prePath = "Content/Subworlds/SubStructures";
         public static string path = "Content/Subworlds/SubStructures/Deepstone";
 
+        #region Enums
         public enum RoomID
         {
             Hallway1,
@@ -70,9 +72,19 @@ namespace UltimateSkyblock.Content.Subworlds.Passes
             RightTreasureRoomTrapped1,
             RightTreasureRoomTrapped2,
             RightTreasureRoomTrapped3,
+
+            Basement1,
+            BasementCatacombCenter,
+            BasementLeftStairs,
+            BasementRightStairs,
+            BasementLeftRuinedDropdown1,
+            BasementLeftRuinedDropdown2,
+            BasementLeftFlooded1,
+            BasementRightRuinedDropdown1,
+            BasementRightRuinedDropdown2,
         }
 
-        public static List<RoomID> hallways = new List<RoomID>
+        public static List<RoomID> Hallways = new List<RoomID>
         {
             RoomID.Hallway1,
             RoomID.Hallway2,
@@ -98,7 +110,7 @@ namespace UltimateSkyblock.Content.Subworlds.Passes
 
         };
 
-        public static List<RoomID> leftRooms = new List<RoomID>
+        public static List<RoomID> LeftRooms = new List<RoomID>
         { 
             RoomID.LeftAlchemyRoom1,
             RoomID.LeftAlchemyRoom2,
@@ -113,7 +125,7 @@ namespace UltimateSkyblock.Content.Subworlds.Passes
             RoomID.LeftSpiderStatueRoom,
         };
 
-        public static List<RoomID> rightRooms = new List<RoomID>
+        public static List<RoomID> RightRooms = new List<RoomID>
         { 
             RoomID.RightLibrary1,
             RoomID.RightLibrary2,
@@ -128,6 +140,22 @@ namespace UltimateSkyblock.Content.Subworlds.Passes
             RoomID.RightTreasureRoomTrapped2,
             RoomID.RightTreasureRoomTrapped3,
         };
+
+        public static List<RoomID> LeftBasementDropdownRooms = new List<RoomID>
+        {
+            RoomID.BasementLeftStairs,
+            RoomID.BasementLeftRuinedDropdown1,
+            RoomID.BasementLeftRuinedDropdown2,
+            RoomID.BasementLeftFlooded1
+        };
+
+        public static List<RoomID> RightBasementDropdownRooms = new List<RoomID>
+        {
+            RoomID.BasementRightRuinedDropdown1,
+            RoomID.BasementRightRuinedDropdown2,
+        };
+
+        #endregion
 
         public static Dictionary<int, string> RoomsDictionary = new Dictionary<int, string>();
             
@@ -144,7 +172,25 @@ namespace UltimateSkyblock.Content.Subworlds.Passes
             Point16 placePoint = new(Main.maxTilesX / 2, Main.UnderworldLayer - 150);
 
             Generator.GenerateStructure(path + "Tower", placePoint, Modref);
+            GenerateBasement(placePoint.X - 12, placePoint.Y + 44);
             HallwayRunner(placePoint.X, placePoint.Y);
+
+            // Fixing previous right room structures that I forgot to place an extra layer of walls to connect hallways.
+            for (int x = 0; x < Main.maxTilesX; x++)
+            {
+                for (int y = Main.UnderworldLayer - 200;  y < Main.maxTilesY; y++)
+                {
+                    Tile tile = Framing.GetTileSafely(x, y);
+                    if (!tile.HasTile && tile.WallType == WallID.None)
+                    {
+                        if (GenUtils.TileLeft(x, y).WallType == WallID.AncientObsidianBrickWall && GenUtils.TileRight(x, y).WallType == WallID.AncientObsidianBrickWall)
+                        {
+                            WorldGen.PlaceWall(x, y, WallID.AncientObsidianBrickWall);
+                            WorldGen.paintWall(x, y, PaintID.GrayPaint, true);
+                        }
+                    } 
+                }
+            }
             
         }
 
@@ -159,45 +205,62 @@ namespace UltimateSkyblock.Content.Subworlds.Passes
             GenerateHallway(x, y + 11, 1, new UnifiedRandom(Environment.TickCount).Next(5, 10));
             GenerateHallway(x, y + 22, -1, new UnifiedRandom(Environment.TickCount).Next(8, 13));
             GenerateHallway(x, y + 22, 1, new UnifiedRandom(Environment.TickCount).Next(8, 13));
+
+            //43
+            //-12
         }
 
+        /// <summary>
+        /// Creates a hallway with the given direction.
+        /// Automatically rolls the hallways and the rooms at the end of the hallway.
+        /// </summary>
+        /// <param name="x">X position of the top left of the tower.</param>
+        /// <param name="y">Y position of the top left of the tower. To generate hallways further down, simply move the given coordinates down a little.</param>
+        /// <param name="dir">Direction. Will throw if not -1 or 1</param>
+        /// <param name="maxRooms">Maximum number of rooms to generate.</param>
+        /// <exception cref="ArgumentException"></exception>
         public void GenerateHallway(int x, int y, int dir, int maxRooms)
         {
             if (dir != -1 && dir != 1)
-                throw new Exception("Generate Hallway - parameter \"dir\" expected to be -1 or 1 only.");
+                throw new ArgumentException("Generate Hallway - parameter \"dir\" expected to be -1 or 1 only.");
 
             List<RoomID> _hallways = RollHallways(maxRooms);
-            RoomID endRoom = RollRoom(dir);
-            int Xoffset = 14 * dir;
-            int currentXOffset = (dir == -1) ? -12 : 16;
-            RoomID currentHallway;
-                
+            RoomID endRoom = RollUpperRoom(dir);
+
+            int Xoffset = 14 * dir; // Inverts the X offset if we are generating to the left. This is needed for the hallway to also generate to the left.
+            int startingOffset = (dir == -1) ? -12 : 16; // Top left of the tower is the expected X and Y, meaning there is some offset for hallways since it's not centered.
+
+            Point16 genPos = new Point16(0, 0);
+
             for (int i = 1; i < _hallways.Count; i++)
             {
-                currentXOffset = Xoffset * i;
+                startingOffset = Xoffset * i;
+
+                // Getting a random hallway to generate, then removing it to prevent repetition.
                 int index = new UnifiedRandom(Environment.TickCount).Next(_hallways.Count);
-                currentHallway = _hallways[index];
+                RoomID currentHallway = _hallways[index];
                 _hallways.RemoveAt(index);
 
-                Point16 pos = new Point16((x + 2) + currentXOffset, y + 14);
-                Generator.GenerateStructure(path + RoomsDictionary[(int)currentHallway], pos, Modref);
+                genPos = new Point16((x + 2) + startingOffset, y + 14);
+                Modref.Logger.Info("Generated hallway - " + currentHallway.ToString() + " at " + genPos);
+                Generator.GenerateStructure(path + RoomsDictionary[(int)currentHallway], genPos, Modref);
             }
 
-            currentXOffset += Xoffset;
-            Point16 posEnd = new Point16(x + 2 + currentXOffset, y + 14);
-            Generator.GenerateStructure(path + RoomsDictionary[(int)endRoom], posEnd, Modref);
+            //Generating the room at the end of the hall.
+            startingOffset += Xoffset;
+            genPos = new Point16(x + 2 + startingOffset, y + 14);
+            Modref.Logger.Info("Generated room - " + endRoom.ToString() + " at " + genPos);
+            Generator.GenerateStructure(path + RoomsDictionary[(int)endRoom], genPos, Modref);
 
         }
 
-        public static RoomID RollRoom(int dir)
-        {
-            return dir == -1 ? leftRooms[new UnifiedRandom(Environment.TickCount).Next(0, leftRooms.Count)] : rightRooms[new UnifiedRandom(Environment.TickCount).Next(0, rightRooms.Count)];
-        }
+        public static RoomID RollUpperRoom(int dir) => dir == -1 ? LeftRooms[new UnifiedRandom(Environment.TickCount).Next(0, LeftRooms.Count)] : RightRooms[new UnifiedRandom(Environment.TickCount).Next(0, RightRooms.Count)];
+        public RoomID RollBasementDropdownRoom(int dir) => dir == -1 ? LeftBasementDropdownRooms[new UnifiedRandom(Environment.TickCount).Next(0, LeftBasementDropdownRooms.Count)] : RightBasementDropdownRooms[new UnifiedRandom(Environment.TickCount).Next(0, RightBasementDropdownRooms.Count)];
 
         public List<RoomID> RollHallways(int numRooms)
         {
             List<RoomID> locHallways = new List<RoomID>();
-            foreach(RoomID room in hallways)
+            foreach(RoomID room in Hallways)
             {
                 locHallways.Add(room);
             }
@@ -207,9 +270,6 @@ namespace UltimateSkyblock.Content.Subworlds.Passes
             for (int i = 0; i < numRooms; i++)
             {
                 int index = new UnifiedRandom(Environment.TickCount).Next(locHallways.Count);
-                Modref.Logger.Info("Index: " + index);
-                Modref.Logger.Info("Rooms: " + numRooms);
-                Modref.Logger.Info("Count: " + locHallways.Count + "\n");
                 RoomID result = locHallways[index];
                 locHallways.RemoveAt(index);
                 output.Add(result);
@@ -218,5 +278,14 @@ namespace UltimateSkyblock.Content.Subworlds.Passes
             return output;
         }
 
+        public void GenerateBasement(int x, int y)
+        {
+            Generator.GenerateStructure(path + RoomID.Basement1.ToString(), new Point16(x, y), Modref);
+            Generator.GenerateStructure(path + RoomsDictionary[(int)RollBasementDropdownRoom(-1)], new Point16(x - 13, y + 3), Modref);
+            Generator.GenerateStructure(path + RoomsDictionary[(int)RollBasementDropdownRoom(1)], new Point16(x + 42, y + 3), Modref);
+            Generator.GenerateStructure(path + RoomID.BasementCatacombCenter.ToString(), new Point16(x, y + 13), Modref);
+
+        }
     }
 }
+

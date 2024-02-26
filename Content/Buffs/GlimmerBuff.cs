2f52ecf7f83b8c73bb3b175a3424fc4bd30665c4
Moo Moo <obsidian1392@gmail.com>
2024-02-26 08:58:35 -0700

    Added glimmer tonic & associated files

---
+++ Content/Buffs/GlimmerBuff.cs
@@ -0,0 +1,27 @@
+﻿using Terraria;
+using Terraria.Graphics.Light;
+using Terraria.ID;
+using Microsoft.Xna.Framework;
+using Terraria.ModLoader;
+
+namespace UltimateSkyblock.Content.Buffs
+{
+    public class GlimmerBuff : ModBuff
+    {
+        public override void Update(Player player, ref int buffIndex)
+        {
+            if (player.HasBuff(BuffID.Darkness))
+            {
+                player.ClearBuff(BuffID.Darkness);
+                player.nightVision = true;
+
+                if (Main.rand.NextBool(10))
+                {
+                    Dust dust = Dust.NewDustDirect(player.position + new Vector2(Main.rand.Next(-5, 5), Main.rand.Next(-5, 5)), 20, 20, DustID.PurpleTorch);
+                    dust.noGravity = true;
+                    dust.noLight = true;
+                }
+            }
+        }
+    }
+}
---
+++ Content/Items/Generic/GlimmerTonic.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using UltimateSkyblock.Content.Buffs;
+using UltimateSkyblock.Content.Items.Placeable;
+
+namespace UltimateSkyblock.Content.Items.Generic
+{
+    public class GlimmerTonic : ModItem
+    {
+
+        public override void SetStaticDefaults()
+        {
+            Item.ResearchUnlockCount = 30;
+
+            ItemID.Sets.DrinkParticleColors[Item.type] = new Color[3] {
+                new Color(224, 116, 214),
+                new Color(121, 84, 229),
+                new Color(0, 30, 156)
+            };
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 16;
+            Item.height = 30;
+            Item.useStyle = ItemUseStyleID.DrinkLiquid;
+            Item.useAnimation = 17;
+            Item.useTime = 17;
+            Item.useTurn = true;
+            Item.UseSound = SoundID.Item3;
+            Item.maxStack = 30;
+            Item.consumable = true;
+            Item.rare = ItemRarityID.Orange;
+            Item.value = Terraria.Item.buyPrice(gold: 1);
+
+            Item.buffType = ModContent.BuffType<GlimmerBuff>();
+            Item.buffTime = 8 * 60 * 60; // too lazy for math
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient(ModContent.ItemType<Glimmercap>())
+                .AddIngredient(ItemID.BottledWater)
+                .AddTile(TileID.Bottles)
+                .Register();
+        }
+    }
+}

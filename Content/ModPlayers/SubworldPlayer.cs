701a5dc05fe7cb04f2da67bb50206a3b7d1011fe
Moo Moo <obsidian1392@gmail.com>
2024-02-26 09:00:07 -0700

    Warning text + commented out flashlight code

--- Content/ModPlayers/SubworldPlayer.cs
+++ Content/ModPlayers/SubworldPlayer.cs
@@ -6,6 +6,7 @@ using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.ModLoader;
 using Terraria.WorldBuilding;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace UltimateSkyblock.Content.ModPlayers
 {
@@ -23,6 +24,28 @@ namespace UltimateSkyblock.Content.ModPlayers
                 Player.ZoneSkyHeight = false;
                 Player.ZoneBeach = false;
                 Main.oceanBG = 0;
+            }
+             
+            // Commented code below is flashlight functionality
+            //if (Player.HeldItem.type == ModContent.ItemType<Content.Items.Placeable.Glimmercap>() && !Framing.GetTileSafely(Main.MouseWorld.ToTileCoordinates().X, Main.MouseWorld.ToTileCoordinates().Y).HasTile)
+            //{
+            //    Lighting.AddLight(Main.MouseWorld, 6, 6, 6);
+            //}
+        }
+
+        //public override void PostUpdate()
+        //{
+        //    if (Player.HeldItem.type == ModContent.ItemType<Content.Items.Placeable.Glimmercap>() && !Framing.GetTileSafely(Main.MouseWorld.ToTileCoordinates().X, Main.MouseWorld.ToTileCoordinates().Y).HasTile)
+        //    {
+        //        Lighting.AddLight(Main.MouseWorld, 6, 6, 6);
+        //    }
+        //}
+
+        public override void OnEnterWorld()
+        {
+            if (SubworldSystem.AnyActive())
+            {
+                Main.NewText("This world is seperate from the main world.\nThis means bosses defeated in here will not sync with the main world, and bosses defeated in the main world will not sync in here.");
             }
         }
     }

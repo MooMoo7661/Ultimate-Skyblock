4805eaf558bf2777cdd0c0394cf72dd30536b002
Moo Moo <obsidian1392@gmail.com>
2024-03-04 17:40:32 -0700

    Added some stuff

--- Utils/SimpleExtensions.cs
+++ Utils/SimpleExtensions.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
 using Terraria;
+using Terraria.ModLoader;
 
 namespace UltimateSkyblock.Content.Utils
 {
@@ -9,21 +11,43 @@ namespace UltimateSkyblock.Content.Utils
         public static float ToSeconds(this float seconds) => seconds * 60;
         public static Vector2 ToVector2(this Tile tile, int x, int y) => new Vector2(x, y);
 
+        public static string ToHexString(this string text, Color color)
+        {
+            return "[c/" + color.Hex3() + ":" + text + "]";
+        }
+
+        public static void AddItemList(this List<Item> list, List<Item> input)
+        {
+            foreach(Item obj in input)
+                list.Add(obj);
+        }
+
         public static void Add(this Chest chest, Item item)
         {
             for (int i = 0; i < chest.item.Length; i++)
             {
-                if (chest.item[i].IsAir)
+                if (chest.item[i].NullOrAir()) // Gets the closest index of a chest that's empty
                 {
-                    chest.item[i] = item;
+                    chest.item[i].SetDefaults(item.type);
+                    chest.item[i].stack = item.stack;
                     return;
                 }
             }
         }
 
-        public static string ToHexString(this string text, Color color)
+        public static void Add(this Chest chest, List<Item> ItemsList)
         {
-            return "[c/" + color.Hex3() + ":" + text + "]";
+            foreach (Item item in ItemsList)
+            {
+                for (int i = 0; i < chest.item.Length; i++)
+                {
+                    if (chest.item[i].NullOrAir())
+                    {
+                        chest.item[i].SetDefaults(item.type);
+                        chest.item[i].stack = item.stack;
+                    }
+                }
+            }
         }
     }
 }

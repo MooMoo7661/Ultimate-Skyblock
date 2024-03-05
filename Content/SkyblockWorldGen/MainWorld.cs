9302fb2f5e5bb160ccc2ba7879d98ec9edf07a20
Moo Moo <obsidian1392@gmail.com>
2024-03-04 17:38:49 -0700

    Wrote explanations for code

--- Content/SkyblockWorldGen/MainWorld.cs
+++ Content/SkyblockWorldGen/MainWorld.cs
@@ -10,14 +10,21 @@ using System;
 using UltimateSkyblock.Content.Utils;
 using Microsoft.Xna.Framework;
 
-namespace UltimateSkyblock.SkyblockWorldGen
+namespace UltimateSkyblock.Content.SkyblockWorldGen
 {
     public partial class MainWorld : ModSystem
     {
-        /// <summary>
-        /// Used with RollHellIslands to pick an island that is not the same as the previous one.
-        /// </summary>
+        //This class is mainly for modifying the world's data, and not actually generating islands.
+        //Island generation can be found in SkyblockWorldGen/IslandsGeneration.cs
+        //Planetoids generation can be found in SkyblockWorldGen/PlanetoidGeneration.cs
+        //Points for islands & planetoids are in SkyblockWorldGen/WorldHelpers.cs
+        
+        //Subworlds & subworld structures are found in Content/Subworlds
+        //All of the subworld generation is split into a bunch of passes for better organization in Subworlds/Passes
+
+        /// <summary> Used with RollHellIslands to pick an island that is not the same as the previous one.</summary>
         public static string previousStructure;
+
         public static float ScaleBasedOnWorldSizeX;
         public static float ScaleBasedOnWorldSizeY;
         public static WorldSizes WorldSize;
@@ -35,11 +42,16 @@ namespace UltimateSkyblock.SkyblockWorldGen
         {
             Classic,
             Simple,
-            Luxury
+            Luxury,
+            None
         }
 
         public override void ModifyWorldGenTasks(List<GenPass> tasks, ref double totalWeight)
         {
+            //Removing a few tasks to try and speed up the worldgen process.
+            //The reason I don't just remove every pass is because many things, like the world layers, backgrounds, etc. rely on those passes being called.
+            //An example of that is for some reason, Jungle Temple bricks become very messed up when the task isn't run.
+
             tasks.RemoveAll(task => task.Name == "Full Desert" || task.Name == "Buried Chests" || task.Name == "Mushroom Patches" ||
             task.Name == "Micro Biomes" || task.Name == "Moss" || task.Name == "Guide");
         }

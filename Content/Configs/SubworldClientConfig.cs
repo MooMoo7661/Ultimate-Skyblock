00e2d0d6b769dd0d4faefe271a7efe163a471f0b
Moo Moo <obsidian1392@gmail.com>
2024-03-13 11:11:12 -0600

    Removed obsolete labels

--- Content/Configs/SubworldClientConfig.cs
+++ Content/Configs/SubworldClientConfig.cs
@@ -10,17 +10,14 @@ namespace UltimateSkyblock.Content.Configs
         public override ConfigScope Mode => ConfigScope.ClientSide;
 
         [Header("WorldGenDebugInfo")]
-        [Label("Show Fast Noise Seed")]
         [DefaultValue(true)]
         [BackgroundColor(0, 255, 0)]
         public bool ShowFastNoiseSeed { get; set; }
 
-        [Label("Show Fractal Type")]
         [DefaultValue(false)]
         [BackgroundColor(0, 255, 0)]
         public bool ShowFractalType { get; set; }
 
-        [Label("Show Perlin Type")]
         [DefaultValue(false)]
         [BackgroundColor(0, 255, 0)]
         public bool ShowPerlinType { get; set; }

d01e59ec6bfb225e15edf81fcbc336fe560f17d0
Moo Moo <obsidian1392@gmail.com>
2024-03-23 23:44:37 -0700

    Join world notification is now delayed by 6 seconds, to allow any other mods that output text on entering to work

--- Content/ModPlayers/SkyblockPlayer.cs
+++ Content/ModPlayers/SkyblockPlayer.cs
@@ -6,13 +6,33 @@ using Terraria.ID;
 using static UltimateSkyblock.Content.SkyblockWorldGen.MainWorld;
 using UltimateSkyblock.Content.Configs;
 using SubworldLibrary;
+using System.Threading;
 
 namespace UltimateSkyblock.Content.ModPlayers
 {
     public class SkyblockPlayer : ModPlayer
     {
+        ushort joinTimer = 0;
+        bool locked = false;
+
         public override void PreUpdate()
         {
+            //Join message to tell the player not to use small worlds.
+            if (!locked)
+            {
+                if (joinTimer == 360)
+                {
+                    if (WorldSize == WorldSizes.Small && ModContent.GetInstance<SkyblockModConfig>().SmallWorldWarning && SubworldSystem.Current == null)
+                    {
+                        Main.NewText("----------" + "\nIt has been detected that this is a small world.\nFor the best experience, please create a medium or large world, as the world generation will be extremely bad and limited.\n[c/E136EE:This message can be disabled at any time through the Gameplay Config.]\n" + "----------");
+                    }
+                    locked = true;
+                }
+                else
+                    joinTimer++;
+            }
+            
+            //Handles teleporting the player to the top of the world when falling out of the bottom.
             if (Player.position.ToTileCoordinates().Y >= Main.maxTilesY - 45 && SubworldSystem.Current == null)
             {
                 if (ModContent.GetInstance<SkyblockModConfig>().TeleportToTopOfWorldOnDeath)
@@ -28,10 +48,8 @@ namespace UltimateSkyblock.Content.ModPlayers
 
         public override void OnEnterWorld()
         {
-            if (WorldSize == WorldSizes.Small && ModContent.GetInstance<SkyblockModConfig>().SmallWorldWarning && SubworldSystem.Current == null)
-            {
-                Main.NewText("[c/E136EE:It has been detected that this is a small world. For the best experience, please create a medium or large world, as the world generation will be extremely bad and limited.]\n[c/E136EE:This message can be disabled at any time through the Gameplay Config.]");
-            }
+            locked = false;
+            joinTimer = 0;
         }
     }
 }

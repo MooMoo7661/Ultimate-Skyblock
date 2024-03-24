d05e08907bb53b7c966b659c33e9ee25a11ddf75
Moo Moo <obsidian1392@gmail.com>
2024-03-23 23:45:32 -0700

    Tweaked heights & added desert fields

--- Content/SkyblockWorldGen/WorldHelpers.cs
+++ Content/SkyblockWorldGen/WorldHelpers.cs
@@ -9,10 +9,11 @@ public static class WorldHelpers
 {
     public static Point16 Hell => new Point16(Main.maxTilesX / 2, Main.UnderworldLayer + 50);
     public static Point16 Hallow;
-    public static Point16 Mushroom => dungeonLeft ? new Point16(Main.maxTilesX - Main.maxTilesX / 20, (int)Main.rockLayer - 400 - (int)(ScaleBasedOnWorldSizeY * 2f)) : new Point16(Main.maxTilesX / 20, (int)Main.rockLayer - 400 - (int)(ScaleBasedOnWorldSizeY * 2f));
+    public static Point16 Mushroom => dungeonLeft ? new Point16(Main.maxTilesX - Main.maxTilesX / 20, Main.maxTilesY / 2 - Main.maxTilesY / 5) : new Point16(Main.maxTilesX / 20, Main.maxTilesY / 2 - Main.maxTilesY / 5);
     public static Point16 Spawn => new(Main.maxTilesX / 2, Main.maxTilesY / 2 - Main.maxTilesY / 5); // Spawn point on the Spawn Island
     public static Point16 Jungle => new(Main.maxTilesX / 2 + Main.maxTilesX / 7 + (int)(ScaleBasedOnWorldSizeX * 2), Main.maxTilesY / 2 - Main.maxTilesY / 5); // Center of the main jungle island
     public static Point16 Evil => new(Main.maxTilesX / 2 - Main.maxTilesX / 7 + (int)(ScaleBasedOnWorldSizeX * 1.3f), 100); // Center to spawn evil islands at
+    public static Point16 Desert => new(Main.maxTilesX / 4, Main.maxTilesY / 2 - Main.maxTilesY / 5);
 
     /// <summary>Bottom left of the Snow islands.</summary>
     public static Point16 Snow => new(Main.maxTilesX / 2 + Main.maxTilesX / 4 + (int) (ScaleBasedOnWorldSizeX* 1.3f), Main.maxTilesY / 2 - Main.maxTilesY / 4);
@@ -27,4 +28,5 @@ public static class WorldHelpers
     public static readonly string crimsonPath = path + "Crimson";
     public static readonly string hivePath = path + "Hive";
     public static readonly string snowPath = path + "SnowIsland";
+    public static readonly string desertPath = path + "DesertIsland";
 }

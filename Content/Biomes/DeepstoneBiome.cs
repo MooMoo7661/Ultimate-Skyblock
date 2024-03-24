73cf43c63fc1d07a55429f0dd1d3185c443071d4
Moo Moo <obsidian1392@gmail.com>
2024-03-23 23:41:14 -0700

    Removed height requirement for the Deepstone Biome to appear

--- Content/Biomes/DeepstoneBiome.cs
+++ Content/Biomes/DeepstoneBiome.cs
@@ -1,15 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Xna.Framework;
-using Terraria;
 using Terraria.Graphics.Capture;
-using Terraria.ID;
-using Terraria.Localization;
-using Terraria.ModLoader;
-using UltimateSkyblock.Content.Items.Placeable;
 using UltimateSkyblock.Content.Tiles.Blocks;
 
 namespace UltimateSkyblock.Content.Biomes
@@ -30,19 +20,17 @@ namespace UltimateSkyblock.Content.Biomes
         public override Color? BackgroundColor => base.BackgroundColor;
         public override string MapBackground => BackgroundPath;
 
-        public override SceneEffectPriority Priority => SceneEffectPriority.BiomeMedium;
+        public override SceneEffectPriority Priority => SceneEffectPriority.BiomeHigh;
 
         public override bool IsBiomeActive(Player player)
         {
-            return ModContent.GetInstance<DeepstoneCount>().blockCount >= 40 &&
-                player.position.ToTileCoordinates().Y > Main.maxTilesY / 2;
+            return ModContent.GetInstance<DeepstoneCount>().blockCount >= 40;
         }
 
         public override void OnInBiome(Player player)
         {
             player.AddBuff(BuffID.Darkness, 301);
         }
-
     }
 
     public class DeepstoneCount : ModSystem

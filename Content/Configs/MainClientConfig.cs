ec858aca22587214037abb6bffd722491b84399a
Moo Moo <obsidian1392@gmail.com>
2024-03-24 10:05:42 -0700

    Tweaked config positions

---
+++ Content/Configs/MainClientConfig.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using Terraria.ModLoader.Config;
+using static UltimateSkyblock.Content.SceneEffects.SkyblockScene_Radio;
+
+namespace UltimateSkyblock.Content.Configs
+{
+    public class MainClientConfig : ModConfig
+    {
+        public override ConfigScope Mode => ConfigScope.ClientSide;
+
+        [Header("Music")]
+        [DefaultValue(true)]
+        public bool OWSoundtrack { get; set; }
+
+        [DefaultValue(-1)]
+        [Slider]
+        [Range(-1, 1)]
+        [Increment(1)]
+        public RadioID RadioSlider { get; set; }
+    }
+}

7006aba1dd3e8982720cf255b0ce0248627d5c3c
Moo Moo <obsidian1392@gmail.com>
2024-03-31 15:59:51 -0600

    Added biome marker tiles

---
+++ Content/Items/Placeable/MapMarkers/JungleBiomeMarkerItem.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+using Microsoft.Xna.Framework;
+using UltimateSkyblock.Content.Tiles.Blocks;
+using UltimateSkyblock.Content.Tiles.Furniture.MapMarkers;
+
+namespace UltimateSkyblock.Content.Items.Placeable.MapMarkers
+{
+    public class JungleBiomeMarkerItem : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Item.ResearchUnlockCount = 1;
+        }
+
+
+        public override void SetDefaults()
+        {
+            Item.width = 28;
+            Item.height = 28;
+            Item.maxStack = 99;
+            Item.value = Item.sellPrice(gold: 1, silver: 22);
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.useTurn = true;
+            Item.useAnimation = 15;
+            Item.useTime = 10;
+            Item.autoReuse = true;
+            Item.consumable = true;
+            Item.createTile = ModContent.TileType<JungleBiomeMarker>();
+            Item.placeStyle = 0;
+        }
+    }
+}
---
+++ Content/Tiles/Furniture/MapMarkers/ForestBiomeMarker.cs
@@ -0,0 +1,98 @@
+﻿using Terraria.Audio;
+using Terraria.Enums;
+using Terraria.GameContent.ObjectInteractions;
+using Terraria.Localization;
+using Terraria.ObjectData;
+using UltimateSkyblock.Content.Items.Placeable;
+using UltimateSkyblock.Content.Tiles.Blocks;
+using UltimateSkyblock.Content.UI.MapDrawing;
+
+namespace UltimateSkyblock.Content.Tiles.Furniture.MapMarkers
+{
+    public class ForestBiomeMarker : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            // Properties
+            Main.tileNoAttach[Type] = true;
+            Main.tileLavaDeath[Type] = true;
+            Main.tileFrameImportant[Type] = true;
+            TileID.Sets.DisableSmartCursor[Type] = true;
+            TileID.Sets.IgnoredByNpcStepUp[Type] = true;
+            TileID.Sets.PreventsSandfall[Type] = true;
+            TileID.Sets.AvoidedByMeteorLanding[Type] = true;
+
+            DustType = DustID.Grass;
+
+            // Placement
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style3x3);
+            TileObjectData.newTile.Width = 3;
+            TileObjectData.newTile.Height = 3;
+            TileObjectData.newTile.StyleHorizontal = true;
+            TileObjectData.newTile.CoordinateHeights = new int[] { 16, 16, 18 };
+            TileObjectData.newTile.HookPostPlaceMyPlayer = new PlacementHook(ModContent.GetInstance<ForestMapMarkerEntity>().Hook_AfterPlacement, -1, 0, false);
+            TileObjectData.newTile.UsesCustomCanPlace = true;
+            TileObjectData.addTile(Type);
+
+            // Etc
+            AddMapEntry(new Color(30, 94, 20), Language.GetText("Mods.UltimateSkyblock.Tiles.ForestMarker.MapEntry"));
+        }
+    }
+
+    public class ForestMapMarkerEntity : ModTileEntity
+    {
+        private MapIcon icon;
+
+        public override int Hook_AfterPlacement(int i, int j, int type, int style, int direction, int alternate)
+        {
+            if (Main.netMode == NetmodeID.MultiplayerClient)
+            {
+                int width = 3;
+                int height = 3;
+                NetMessage.SendTileSquare(Main.myPlayer, i, j, width, height);
+                NetMessage.SendData(MessageID.TileEntityPlacement, number: i, number2: j, number3: Type);
+            }
+
+            // ModTileEntity.Place() handles checking if the entity can be placed, then places it for you
+            // Set "tileOrigin" to the same value you set TileObjectData.newTile.Origin to in the ModTile
+            Point16 tileOrigin = new Point16(1, 1);
+            int placedEntity = Place(i - tileOrigin.X, j - tileOrigin.Y);
+            return placedEntity;
+        }
+
+        public override bool IsTileValidForEntity(int x, int y)
+        {
+            var tile = Main.tile[x, y];
+            return tile.HasTile && tile.TileType == ModContent.TileType<ForestBiomeMarker>();
+        }
+
+
+        public override void OnNetPlace()
+        {
+            if (Main.netMode == NetmodeID.Server)
+            {
+                NetMessage.SendData(MessageID.TileEntitySharing, number: ID, number2: Position.X, number3: Position.Y);
+            }
+        }
+
+        public override void Update()
+        {
+            int i = Position.X;
+            int j = Position.Y;
+            if (!Framing.GetTileSafely(i, j).HasTile)
+            {
+                Kill(i, j);
+            }
+
+            Texture2D forest = ModContent.Request<Texture2D>("UltimateSkyblock/Content/UI/MapDrawing/Icons/IconForest").Value;
+            icon = new MapIcon(new(Position.X + 1.5f, Position.Y), forest, Color.White, 1.1f, 0.8f, "Forest Marker");
+            TileIconDrawing.icons.Add(icon);
+        }
+
+        public override void OnKill()
+        {
+            TileIconDrawing.icons.Remove(icon);
+        }
+    }
+}
+

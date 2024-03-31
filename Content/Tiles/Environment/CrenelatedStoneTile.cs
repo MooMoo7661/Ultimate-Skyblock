45f37d93e26304ea98c1a1b4a5a15f90ca3079f8
Moo Moo <obsidian1392@gmail.com>
2024-03-31 16:07:08 -0600

    Added replacements for Hellstone Brick walls & tiles to stop dust and fire damage

---
+++ Content/Tiles/Environment/CrenelatedStoneTile.cs
@@ -0,0 +1,32 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+using Microsoft.Xna.Framework;
+using Terraria.DataStructures;
+using Terraria.ObjectData;
+using Microsoft.Xna.Framework.Graphics;
+using UltimateSkyblock.Content.Configs;
+using UltimateSkyblock.Content.Items.Placeable;
+
+namespace UltimateSkyblock.Content.Tiles.Environment
+{
+    public class CrenelatedStoneTile : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.tileMerge[Type][Type] = true;
+            Main.tileSolid[Type] = true;
+            Main.tileMerge[Type][TileID.Stone] = true;
+            Main.tileMerge[TileID.Stone][Type] = true;
+            Main.tileLighted[Type] = false;
+            Main.tileNoSunLight[Type] = false;
+            Main.tileBlockLight[Type] = true;
+            TileID.Sets.Stone[Type] = true;
+            DustType = DustID.Stone;
+            HitSound = SoundID.Tink;
+            RegisterItemDrop(ItemID.StoneBlock);
+
+            AddMapEntry(new Color(128, 128, 128));
+        }
+    }
+}
---
+++ Content/Tiles/Environment/CrenelatedStoneWallTile.cs
@@ -0,0 +1,30 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+using Microsoft.Xna.Framework;
+using Terraria.DataStructures;
+using Terraria.ObjectData;
+using Microsoft.Xna.Framework.Graphics;
+using UltimateSkyblock.Content.Configs;
+using UltimateSkyblock.Content.Items.Placeable;
+
+namespace UltimateSkyblock.Content.Tiles.Environment
+{
+    public class CrenelatedStoneWallTile : ModWall
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.wallHouse[Type] = false;
+
+            DustType = DustID.Stone;
+
+            AddMapEntry(new Color(112, 112, 112));
+            RegisterItemDrop(WallID.Stone);
+        }
+
+        public override void NumDust(int i, int j, bool fail, ref int num)
+        {
+            num = fail ? 1 : 3;
+        }
+    }
+}

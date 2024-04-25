88c05f133391ecf073f7a3143b2ea2533a545a39
Moo Moo <obsidian1392@gmail.com>
2024-04-25 00:01:20 -0600

    namespace changes

--- Content/Items/Generic/GlimmerTonic.cs
+++ Content/Items/Generic/GlimmerTonic.cs
@@ -1,16 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Xna.Framework;
-using Terraria;
-using Terraria.ID;
-using Terraria.Localization;
-using Terraria.ModLoader;
-using UltimateSkyblock.Content.Buffs;
-using UltimateSkyblock.Content.Items.Placeable;
-
+﻿using UltimateSkyblock.Content.Buffs;
 namespace UltimateSkyblock.Content.Items.Generic
 {
     public class GlimmerTonic : ModItem

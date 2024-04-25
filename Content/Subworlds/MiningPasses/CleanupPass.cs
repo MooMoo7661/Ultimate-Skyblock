f3d9d5a5a32de2428a0e319cff14fd2c135a3045
Moo Moo <obsidian1392@gmail.com>
2024-04-25 00:07:15 -0600

    comment tweak

--- Content/Subworlds/MiningPasses/CleanupPass.cs
+++ Content/Subworlds/MiningPasses/CleanupPass.cs
@@ -9,6 +9,7 @@ using Terraria.IO;
 using Terraria.WorldBuilding;
 using UltimateSkyblock.Content.Items.Placeable;
 using static UltimateSkyblock.Content.Subworlds.MiningSubworld;
+using UltimateSkyblock.Content.Tiles.Blocks;
 
 namespace UltimateSkyblock.Content.Subworlds.MiningPasses
 {
@@ -25,13 +26,13 @@ namespace UltimateSkyblock.Content.Subworlds.MiningPasses
                 {
                     Tile tile = Framing.GetTileSafely(x, y);
 
-                    //Removing any extra water droplets that may have generated on sloped tiles.
-                    if (tile.HasTile && tile.Slope != SlopeType.Solid && Framing.GetTileSafely(x, y + 1).TileType == TileID.WaterDrip)
+                    //Removing any extra water or lava droplets that may have generated on sloped tiles.
+                    if (tile.HasTile && tile.Slope != SlopeType.Solid && (Framing.GetTileSafely(x, y + 1).TileType == TileID.WaterDrip || Framing.GetTileSafely(x, y + 1).TileType == TileID.LavaDrip))
                     {
                         Framing.GetTileSafely(x, y + 1).ClearTile();
                     }
                     progress.Set((y + x * Main.maxTilesY) / (float)(Main.maxTilesX * Main.maxTilesY));
-                }
+                }  
             }
         }
     }

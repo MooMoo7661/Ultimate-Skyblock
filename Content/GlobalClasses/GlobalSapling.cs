f12d3777be4f831625410b460df7dfce630ed19e
Moo Moo <obsidian1392@gmail.com>
2024-04-25 00:00:15 -0600

    Moved sapling item code to GlobalItem.Drop

--- Content/GlobalClasses/GlobalSapling.cs
+++ Content/GlobalClasses/GlobalSapling.cs
@@ -5,6 +5,12 @@ namespace UltimateSkyblock.Content.GlobalClasses
 {
     public class GlobalSapling : GlobalTile
     {
+        public override void Drop(int i, int j, int type)
+        {
+           if (type == TileID.Saplings || type == TileID.GemSaplings)
+                Item.NewItem(GetItemSource_FromTileBreak(i, j), new Vector2(i * 16, j * 16), Vector2.Zero, ItemID.Acorn);
+        }
+
         public override void RandomUpdate(int i, int j, int type)
         {
             var config = ModContent.GetInstance<SkyblockModConfig>();
@@ -20,14 +26,7 @@ namespace UltimateSkyblock.Content.GlobalClasses
 
         public override void KillTile(int i, int j, int type, ref bool fail, ref bool effectOnly, ref bool noItem)
         {
-            Tile tile = Main.tile[i, j];
-
-            if (type == TileID.Saplings && !fail && tile.TileFrameY != 0 && ModContent.GetInstance<SkyblockModConfig>().SaplingsDropAcorns)
-            {
-                Item.NewItem(GetItemSource_FromTileBreak(i, j), new Vector2(i * 16, j * 16), Vector2.Zero, ItemID.Acorn);
-            }
-
-            if ((type == TileID.Crystals || type == TileID.Titanstone) && !Main.hardMode)
+            if ((type == TileID.Crystals || type == TileID.Titanstone || type == TileID.MythrilBrick || type == TileID.CobaltBrick) && !Main.hardMode)
             {
                 noItem = true;
             }

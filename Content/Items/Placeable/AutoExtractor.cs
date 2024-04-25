efad50f667321f1a31699849437c88cecf15bfa1
Moo Moo <obsidian1392@gmail.com>
2024-04-25 00:02:27 -0600

    Added temporary global null tile for structure helper ease of use

--- Content/Items/Placeable/AutoExtractor.cs
+++ Content/Items/Placeable/AutoExtractor.cs
@@ -3,6 +3,9 @@ using Terraria.ModLoader;
 using Terraria;
 using UltimateSkyblock.Content.ModSystems;
 using UltimateSkyblock.Content.Tiles.Extractinators;
+using UltimateSkyblock.Content.Tiles.Furniture;
+using Terraria.ObjectData;
+using Terraria.Enums;
 
 namespace UltimateSkyblock.Content.Items.Placeable
 {
@@ -22,7 +25,7 @@ namespace UltimateSkyblock.Content.Items.Placeable
             Item.autoReuse = true;
             Item.useStyle = ItemUseStyleID.Swing;
             Item.maxStack = Item.CommonMaxStack;
-            Item.createTile = ModContent.TileType<AutoExtractorTier1Tile>();
+            Item.createTile = ModContent.TileType<SunshinePainting>();
             Item.rare = ItemRarityID.Green;
             Item.useTime = 10;
             Item.useTurn = true;
@@ -38,6 +41,39 @@ namespace UltimateSkyblock.Content.Items.Placeable
                 .AddIngredient(ItemID.Diamond, 3)
                 .AddTile(TileID.Anvils)
                 .Register();
+        }
+    }
+
+    public class GlobalNullTile : GlobalTile
+    {
+        public override void SetStaticDefaults()
+        {
+            if (ModLoader.TryGetMod("StructureHelper", out Mod structureHelper))
+            {
+                if (structureHelper.TryFind<ModTile>("NullBlock", out ModTile nullTile))
+                {
+                    Main.tileSolid[nullTile.Type] = false;
+                    Main.tileBlockLight[nullTile.Type] = false;
+                    Main.tileNoSunLight[nullTile.Type] = false;
+
+                    TileObjectData.newTile.CopyFrom(TileObjectData.Style1x1);
+                    TileObjectData.newTile.AnchorBottom = new AnchorData(AnchorType.None, count:0, start: 0);
+                    TileObjectData.addTile(nullTile.Type);
+                }
+            }
+        }
+
+        public override bool CreateDust(int i, int j, int type, ref int dustType)
+        {
+            if (ModLoader.TryGetMod("StructureHelper", out Mod structureHelper))
+            {
+                if (structureHelper.TryFind<ModTile>("NullBlock", out _))
+                {
+                    return false;
+                }
+            }
+
+            return base.CreateDust(i , j, type, ref dustType);
         }
     }
 }

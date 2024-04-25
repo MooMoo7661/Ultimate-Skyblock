0cf7b545647883eea9733ce76f2a72c829470318
Moo Moo <obsidian1392@gmail.com>
2024-04-25 00:05:40 -0600

    Added Dungeon Subworld generation + class for using structure helper's internals

---
+++ Content/Subworlds/DungeonPasses/DungeonGenerationPass.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StructureHelper;
+using Terraria.ModLoader.IO;
+using Terraria.ModLoader;
+using UltimateSkyblock.Content.Subworlds.DungeonRoomUtils;
+using static UltimateSkyblock.Content.Subworlds.DungeonRoomUtils.DungeonRoom;
+using System.Reflection;
+
+namespace UltimateSkyblock.Content.Subworlds.DungeonPasses
+{
+    public class DungeonGenerationPass : GenPass
+    {
+        public DungeonGenerationPass(string name, double loadWeight) : base(name, loadWeight) { }
+
+        public static int previousRoom = -1;
+        public static string prePath = "Content/Subworlds/DungeonStructures/";
+
+        protected override void ApplyPass(GenerationProgress progress, GameConfiguration configuration)
+        {
+            Point16 dims = Point16.Zero;
+            Generator.GetDimensions(prePath + "DungeonEntrance", UltimateSkyblock.Instance, ref dims);
+            Point16 MainPoint = new Point16(Main.maxTilesX / 2 - dims.X / 2, Main.maxTilesY / 2);
+
+            Generator.GenerateMultistructureRandom(prePath + "DungeonEntrance", MainPoint, UltimateSkyblock.Instance);
+            HallwayRunner(new Point16(MainPoint.X, MainPoint.Y));
+
+            Generator.GetMultistructureDimensions(prePath + "DungeonEntrance", UltimateSkyblock.Instance, 0, ref dims);
+            Point spawn = new(MainPoint.X + dims.X / 2, MainPoint.Y + dims.Y - 6);
+            Main.spawnTileX = spawn.X;
+            Main.spawnTileY = spawn.Y;
+        }
+
+        public void HallwayRunner(Point16 genPoint)
+        {
+            GenerateBottomHallway(genPoint.X, genPoint.Y + 34, -1, 4);
+            GenerateBottomHallway(genPoint.X + 67, genPoint.Y + 34, 1, 4);
+        }
+
+        public void GenerateBottomHallway(int x, int y, int dir, int maxRooms)
+        {
+            int offset = 0;
+            previousRoom = -1;
+
+            for (int i = 0; i < maxRooms; i++)
+            {
+                int index = RollRoom(i);
+                Point16 dims = Point16.Zero;
+                Generator.GetMultistructureDimensions(prePath + "DungeonRooms", UltimateSkyblock.Instance, index, ref dims);
+
+                //When generating to the right, only offset AFTER generation
+                if (dir == 1)
+                {
+                    Generator.GenerateMultistructureSpecific(prePath + "DungeonRooms", new Point16(x + offset, y - dims.Y), UltimateSkyblock.Instance, index);
+                    offset += dims.X;
+                }
+                else
+                {
+                    offset += dims.X * dir;
+                    Generator.GenerateMultistructureSpecific(prePath + "DungeonRooms", new Point16(x + offset, y - dims.Y), UltimateSkyblock.Instance, index);
+                }
+            }
+        }
+
+        int RollRoom(int randIndex, int iterations = 0)
+        {
+            TagCompound tag = DynamicStructureSystem.GetTag(prePath + "DungeonRooms", UltimateSkyblock.Instance);
+            List<TagCompound> list = (List<TagCompound>)tag.GetList<TagCompound>("Structures");
+            int seed = DateTime.Now.Millisecond + randIndex * iterations;
+            int index = new UnifiedRandom(seed).Next(list.Count);
+            UltimateSkyblock.Instance.Logger.Info("Seed: " + seed);
+
+            UltimateSkyblock.Instance.Logger.Info("Rolled index: " + index + ", previous index: " + previousRoom);
+            if (index == previousRoom)
+                return RollRoom(randIndex, iterations++);
+            else
+            {
+                previousRoom = index;
+                return index;
+            }
+        }
+    }
+}
---
+++ Content/Subworlds/DungeonRoomUtils/DynamicStructureSystem.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader.IO;
+
+namespace UltimateSkyblock.Content.Subworlds.DungeonRoomUtils
+{
+    public class DynamicStructureSystem
+    {
+        //This exists because anything related to getting info about a structure file is internal in Structure Helper, which I may add, is incredibly annoying.
+        //So yeah I just copied and pasted everything I needed here.
+
+        public static Dictionary<string, TagCompound> DynamicStructureDataCache = new Dictionary<string, TagCompound>();
+
+        public static bool LoadFile(string path, Mod mod, bool fullPath = false)
+        {
+            TagCompound tagCompound;
+            if (!fullPath)
+            {
+                Stream fileStream = mod.GetFileStream(path);
+                tagCompound = TagIO.FromStream(fileStream);
+                fileStream.Close();
+            }
+            else
+            {
+                tagCompound = TagIO.FromFile(path);
+            }
+
+            DynamicStructureDataCache.Add(path, tagCompound);
+            return true;
+        }
+
+        public static TagCompound GetTag(string path, Mod mod, bool fullPath = false)
+        {
+            if (!DynamicStructureDataCache.ContainsKey(path))
+            {
+                if (!LoadFile(path, mod, fullPath))
+                {
+                    return null;
+                }
+
+                return DynamicStructureDataCache[path];
+            }
+
+            return DynamicStructureDataCache[path];
+        }
+
+        public static bool GetDimensions(string path, Mod mod, ref Point16 dims, bool fullPath = false)
+        {
+            TagCompound tag = GetTag(path, mod, fullPath);
+            dims = new Point16(tag.GetInt("Width") + 1, tag.GetInt("Height") + 1);
+            return true;
+        }
+    }
+}

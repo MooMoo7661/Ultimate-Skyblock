63189657f9ad21b3eae55c0c68756b92168b9d57
Moo Moo <obsidian1392@gmail.com>
2024-05-07 09:52:07 -0600

    Removed world position helpers to facilitate the new (and much better) slice system.

--- Content/SkyblockWorldGen/WorldHelpers.cs
+++ Content/SkyblockWorldGen/WorldHelpers.cs
@@ -1,15 +1,6 @@
 ﻿namespace UltimateSkyblock.Content.SkyblockWorldGen;
 public static class WorldHelpers
 {
-    public static Point16 Hell;
-    public static Point16 Mushroom;
-    public static Point16 Spawn;
-    public static Point16 Jungle;
-    public static Point16 Evil;
-    public static Point16 Desert;
-    public static Point16 Hallow;
-    public static Point16 Snow;
-
     // Quick ref paths
     public static readonly string path = "Content/SkyblockWorldGen/Structures/";
     public static readonly string hellPath = path + "HellIsland";

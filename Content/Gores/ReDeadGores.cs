90c6a8a089f18237bcf04742e71fa91b62715e84
Moo Moo <obsidian1392@gmail.com>
2024-05-07 09:38:34 -0600

    Added gores for re-deads

---
+++ Content/Gores/ReDeadGores.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UltimateSkyblock.Content.Gores
+{
+    public class ReDeadGores_Head_Inactive : ModGore
+    {
+        public override string Texture => "UltimateSkyblock/Content/Gores/Head_Inactive";
+    }
+
+    public class ReDeadGores_Head_Active : ModGore
+    {
+        public override string Texture => "UltimateSkyblock/Content/Gores/Head_Active";
+    }
+
+    public class ReDeadGores_Arm_Left : ModGore
+    {
+        public override string Texture => "UltimateSkyblock/Content/Gores/Arm_Left";
+    }
+
+    public class ReDeadGores_Arm_Right : ModGore
+    {
+        public override string Texture => "UltimateSkyblock/Content/Gores/Arm_Right";
+    }
+
+    public class ReDeadGores_Chest : ModGore
+    {
+        public override string Texture => "UltimateSkyblock/Content/Gores/Chest";
+    }
+
+    public class ReDeadGores_Leg_Left : ModGore
+    {
+        public override string Texture => "UltimateSkyblock/Content/Gores/Leg_Left";
+    }
+
+    public class ReDeadGores_Leg_Right : ModGore
+    {
+        public override string Texture => "UltimateSkyblock/Content/Gores/Leg_Right";
+    }
+
+}

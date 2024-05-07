0e7a4959de901cdd978b1a8fa71ecfaae7d8d98d
Moo Moo <obsidian1392@gmail.com>
2024-05-07 09:58:49 -0600

    Added deepstone brick wall

---
+++ Content/Tiles/Walls/DeepstoneBrickWall.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UltimateSkyblock.Content.Tiles.Walls
+{
+    public class DeepstoneBrickWall : ModWall
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.wallHouse[Type] = true;
+            TileID.Sets.WallsMergeWith[Type] = true;
+            Main.wallLargeFrames[Type] = 1;
+        }
+    }
+}

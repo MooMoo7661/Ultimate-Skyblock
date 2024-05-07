eb9007157febdede9093b8dae551b7a007d74330
Moo Moo <obsidian1392@gmail.com>
2024-05-07 09:38:20 -0600

    Added system for preventing bosses from spawning in subworlds

---
+++ Content/GlobalClasses/SubworldBossPrevention.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SubworldLibrary;
+using UltimateSkyblock.Content.Subworlds;
+
+namespace UltimateSkyblock.Content.GlobalClasses
+{
+    public class SubworldBossPrevention : GlobalNPC
+    {
+        public override void OnSpawn(NPC npc, IEntitySource source)
+        {
+            if (npc.boss && (SubworldSystem.IsActive<MiningSubworld>() || SubworldSystem.IsActive<DungeonSubworld>()))
+                npc.active = false;
+        }
+    }
+}

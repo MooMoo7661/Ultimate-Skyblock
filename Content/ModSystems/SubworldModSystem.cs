0a8f2e2a80ac5aafe5542a60571e88bdd837efe9
Moo Moo <obsidian1392@gmail.com>
2024-05-07 09:42:24 -0600

    Tag syncing for Dungeon Subworld

--- Content/ModSystems/SubworldModSystem.cs
+++ Content/ModSystems/SubworldModSystem.cs
@@ -1,4 +1,5 @@
 using SubworldLibrary;
+using Terraria.ModLoader.IO;
 using UltimateSkyblock.Content.Subworlds;
 
 namespace UltimateSkyblock.Content.ModSystems
@@ -26,6 +27,34 @@ namespace UltimateSkyblock.Content.ModSystems
                     Liquid.UpdateLiquid();
                     Liquid.skipCount = 0;
                 }
+            }
+        }
+
+        public override void SaveWorldData(TagCompound tag)
+        {
+            tag["UltimateSkyblock:PlanteraDungeon"] = NPC.downedPlantBoss;
+            tag["UltimateSkyblock:DownedGolem"] = NPC.downedGolemBoss;
+            tag["UltimateSkyblock:DownedBoss3"] = NPC.downedBoss3;
+            tag["UltimateSkyblock:Hardmode"] = Main.hardMode;
+        }
+
+        public override void LoadWorldData(TagCompound tag)
+        {
+            NPC.downedPlantBoss = tag.GetBool("UltimateSkyblock:PlanteraDungeon");
+            NPC.downedGolemBoss = tag.GetBool("UltimateSkyblock:DownedGolem");
+            NPC.downedBoss3 = tag.GetBool("UltimateSkyblock:DownedBoss3");
+            Main.hardMode = tag.GetBool("UltimateSkyblock:Hardmode");
+        }
+
+        public override void PreUpdateTime()
+        {
+            if (SubworldSystem.AnyActive())
+            {
+                Main.pumpkinMoon = false;
+                Main.snowMoon = false;
+                Main.raining = false;
+                Main.dayTime = true;
+                Main.eclipse = false;
             }
         }
     }

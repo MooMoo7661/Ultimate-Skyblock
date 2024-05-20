8cdef1c60accf7f85d7c24c1f279b454d465cac9
Moo Moo <obsidian1392@gmail.com>
2024-05-20 12:14:51 -0600

    Checks to make sure changes only apply in skyblock

--- Content/GlobalClasses/GlobalSapling.cs
+++ Content/GlobalClasses/GlobalSapling.cs
@@ -7,7 +7,7 @@ namespace UltimateSkyblock.Content.GlobalClasses
     {
         public override void Drop(int i, int j, int type)
         {
-           if (type == TileID.Saplings || type == TileID.GemSaplings)
+           if (type == TileID.Saplings && UltimateSkyblock.IsSkyblock())
                 Item.NewItem(GetItemSource_FromTileBreak(i, j), new Vector2(i * 16, j * 16), Vector2.Zero, ItemID.Acorn);
         }
 
@@ -15,7 +15,7 @@ namespace UltimateSkyblock.Content.GlobalClasses
         {
             var config = ModContent.GetInstance<SkyblockModConfig>();
 
-            if ((type == TileID.Saplings || type == TileID.GemSaplings) && config.FastTrees)
+            if ((type == TileID.Saplings || type == TileID.GemSaplings) && config.FastTrees && UltimateSkyblock.IsSkyblock())
             {
                 if (Main.rand.NextBool(4))
                 {
@@ -26,7 +26,7 @@ namespace UltimateSkyblock.Content.GlobalClasses
 
         public override void KillTile(int i, int j, int type, ref bool fail, ref bool effectOnly, ref bool noItem)
         {
-            if ((type == TileID.Crystals || type == TileID.Titanstone || type == TileID.MythrilBrick || type == TileID.CobaltBrick) && !Main.hardMode)
+            if ((type == TileID.Crystals || type == TileID.Titanstone || type == TileID.MythrilBrick || type == TileID.CobaltBrick) && !Main.hardMode && UltimateSkyblock.IsSkyblock())
             {
                 noItem = true;
             }

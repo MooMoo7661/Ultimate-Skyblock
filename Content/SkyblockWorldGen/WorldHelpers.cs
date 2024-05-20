565bcec8c5587c8c0fbd94552e0b19d39826c076
Moo Moo <obsidian1392@gmail.com>
2024-05-20 12:30:58 -0600

    added get for evil path to return crimson or corruption

--- Content/SkyblockWorldGen/WorldHelpers.cs
+++ Content/SkyblockWorldGen/WorldHelpers.cs
@@ -3,12 +3,19 @@ public static class WorldHelpers
 {
     // Quick ref paths
     public static readonly string path = "Content/SkyblockWorldGen/Structures/";
+
+    public static string evilPath
+    {
+        get
+        {
+            return "Content/SkyblockWorldGen/Structures/" + (WorldGen.crimson? "CrimsonIsland" : "CorruptIsland");
+        }
+    }
+
     public static readonly string hellPath = path + "HellIsland";
     public static readonly string forestPath = path + "ForestIsland";
     public static readonly string junglePath = path + "JungleIsland";
     public static readonly string templePath = path + "Temple";
-    public static readonly string corruptPath = path + "Corrupt";
-    public static readonly string crimsonPath = path + "Crimson";
     public static readonly string hivePath = path + "Hive";
     public static readonly string snowPath = path + "SnowIsland";
     public static readonly string desertPath = path + "DesertIsland";

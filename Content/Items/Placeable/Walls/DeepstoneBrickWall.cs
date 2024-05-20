548c7a49b173a3c6267f84549e1cc5cb03a26c14
Moo Moo <obsidian1392@gmail.com>
2024-05-20 12:28:26 -0600

    Added deepstone brick wall

---
+++ Content/Items/Placeable/Walls/DeepstoneBrickWall.cs
@@ -0,0 +1,41 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+using UltimateSkyblock.Content.Tiles.Blocks;
+using UltimateSkyblock.Content.Tiles.Walls;
+using UltimateSkyblock.Content.Items.Placeable.Tiles;
+
+namespace UltimateSkyblock.Content.Items.Placeable.Walls
+{
+    public class DeepstoneBrickWall : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Item.ResearchUnlockCount = 30;
+        }
+        public override void SetDefaults()
+        {
+            Item.width = 16;
+            Item.height = 16;
+            Item.placeStyle = 0;
+            Item.consumable = true;
+            Item.autoReuse = true;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.maxStack = Item.CommonMaxStack;
+            Item.createWall = ModContent.WallType<DeepstoneBrickWallTile>();
+            Item.rare = ItemRarityID.White;
+            Item.useTime = 10;
+            Item.useTurn = true;
+            Item.useAnimation = 15;
+            ItemID.Sets.ShimmerTransformToItem[Type] = ItemID.StoneSlabWall;
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe(4)
+                .AddIngredient(ModContent.ItemType<DeepstoneBrick>())
+                .AddTile(TileID.WorkBenches)
+                .Register();
+        }
+    }
+}

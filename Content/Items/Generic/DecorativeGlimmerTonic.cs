daa32a97a41f17668e1e2ab453047951b25a782e
Moo Moo <obsidian1392@gmail.com>
2024-05-20 12:19:31 -0600

    Added decorative tonic

---
+++ Content/Items/Generic/DecorativeGlimmerTonic.cs
@@ -0,0 +1,38 @@
+﻿using UltimateSkyblock.Content.Buffs;
+using UltimateSkyblock.Content.Tiles.Extractinators;
+using UltimateSkyblock.Content.Tiles.Furniture;
+namespace UltimateSkyblock.Content.Items.Generic
+{
+    public class DecorativeGlimmerTonic : ModItem
+    {
+
+        public override void SetStaticDefaults()
+        {
+            Item.ResearchUnlockCount = 5;
+        }
+        public override void SetDefaults()
+        {
+            Item.width = 16;
+            Item.height = 16;
+
+            Item.placeStyle = 0;
+            Item.consumable = true;
+            Item.autoReuse = true;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.maxStack = Item.CommonMaxStack;
+            Item.createTile = ModContent.TileType<GlimmerTonicDecoration>();
+            Item.rare = ItemRarityID.Blue;
+            Item.useTime = 10;
+            Item.useTurn = true;
+            Item.useAnimation = 15;
+        }
+
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient(ModContent.ItemType<GlimmerTonic>())
+                .Register();
+        }
+    }
+}
---
+++ Content/Tiles/Furniture/GlimmerTonicDecoration.cs
@@ -0,0 +1,43 @@
+﻿using Terraria.ID;
+using Terraria;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using UltimateSkyblock.Content.Subworlds;
+using UltimateSkyblock.Content.Tiles.Environment.Foliage;
+using UltimateSkyblock.Content.Items.Placeable.Tiles;
+using UltimateSkyblock.Content.Items.Generic;
+using Terraria.ObjectData;
+using Terraria.Enums;
+
+namespace UltimateSkyblock.Content.Tiles.Furniture;
+
+public class GlimmerTonicDecoration : ModTile
+{
+    public override void SetStaticDefaults()
+    {
+        Main.tileNoAttach[Type] = true;
+        Main.tileLavaDeath[Type] = true;
+        Main.tileFrameImportant[Type] = true;
+        Main.tileNoFail[Type] = true;
+        Main.tileSolid[Type] = false;
+        
+        TileID.Sets.AvoidedByMeteorLanding[Type] = true;
+
+        DustType = DustID.Glass;
+
+        HitSound = SoundID.Shatter;
+        RegisterItemDrop(ModContent.ItemType<GlimmerTonic>());
+        RegisterItemDrop(ModContent.ItemType<GlimmerTonic>(), 0);
+        RegisterItemDrop(ModContent.ItemType<GlimmerTonic>(), 1);
+
+        //TileObjectData.newTile.FullCopyFrom(TileID.Bottles);
+        TileObjectData.newTile.CopyFrom(TileObjectData.Style1x2);
+        TileObjectData.newTile.DrawYOffset = 0;
+        TileObjectData.newTile.CoordinateHeights = new int[] { 16, 16 };
+        TileObjectData.newTile.UsesCustomCanPlace = true;
+        TileObjectData.newTile.AnchorBottom = new AnchorData(AnchorType.PlatformNonHammered | AnchorType.SolidWithTop, 1, 0);
+        TileObjectData.addTile(Type);
+
+        AddMapEntry(new Color(63, 31, 24));
+    }
+}

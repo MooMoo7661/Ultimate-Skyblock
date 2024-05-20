a2cb156debc9f42e074207e9ab47914afa6a8458
Moo Moo <obsidian1392@gmail.com>
2024-05-20 12:45:22 -0600

    changed ore generation rarities & sizes

--- Content/Subworlds/MiningPasses/OreGenerationPass.cs
+++ Content/Subworlds/MiningPasses/OreGenerationPass.cs
@@ -23,19 +23,19 @@ namespace UltimateSkyblock.Content.Subworlds.MiningPasses
             LoopWorldAndGenerateTilesWithDepthModifiers(8, strength: Main.rand.Next(9, 18), steps: Main.rand.Next(8, 22), type: TileID.Sandstone, tilesThatCanBeGeneratedOn: new List<int> { MiningSubworld.Slate, TileID.Stone }, levelToDisperse: Main.UnderworldLayer, canGenerateAfterLevel: false);
 
             UltimateSkyblock.Instance.Logger.Info("Generating Dirt");
-            LoopWorldAndGenerateTilesWithDepthModifiers(3, Main.rand.Next(8, 13), Main.rand.Next(14, 22), type: TileID.Dirt, new List<int> { TileID.Stone, MiningSubworld.Slate }, Main.maxTilesY - (Main.maxTilesY / 4), false, 200);
+            LoopWorldAndGenerateTilesWithDepthModifiers(2, Main.rand.Next(4, 14), Main.rand.Next(14, 31), type: TileID.Dirt, new List<int> { TileID.Stone, MiningSubworld.Slate }, Main.maxTilesY / 2, true, 500);
 
             UltimateSkyblock.Instance.Logger.Info("Generating Copper or Tin");
-            LoopWorldAndGenerateTilesWithDepthModifiers(8, Main.rand.Next(6, 10), Main.rand.Next(22, 30), SubVars.copper, new List<int> { TileID.Stone, MiningSubworld.Slate }, Main.UnderworldLayer - 100, false, 100);
+            LoopWorldAndGenerateTilesWithDepthModifiers(6, Main.rand.Next(4, 7), Main.rand.Next(16, 19), SubVars.copper, new List<int> { TileID.Stone, MiningSubworld.Slate }, Main.UnderworldLayer - 100, false, 100);
 
             UltimateSkyblock.Instance.Logger.Info("Generating Silver or Tungsten");
-            LoopWorldAndGenerateTilesWithDepthModifiers(10, Main.rand.Next(4, 8), Main.rand.Next(17, 25), SubVars.silver, new List<int> { TileID.Stone, MiningSubworld.Slate }, Main.UnderworldLayer - 100, false, 100);
+            LoopWorldAndGenerateTilesWithDepthModifiers(7, Main.rand.Next(4, 7), Main.rand.Next(14, 19), SubVars.silver, new List<int> { TileID.Stone, MiningSubworld.Slate }, Main.UnderworldLayer - 100, false, 100);
 
             UltimateSkyblock.Instance.Logger.Info("Generating Iron or Lead");
-            LoopWorldAndGenerateTilesWithDepthModifiers(9, Main.rand.Next(7, 13), Main.rand.Next(4, 8), SubVars.iron, new List<int> { TileID.Stone, MiningSubworld.Slate }, Main.UnderworldLayer - 100, false, 100);
+            LoopWorldAndGenerateTilesWithDepthModifiers(6, Main.rand.Next(6, 10), Main.rand.Next(4, 8), SubVars.iron, new List<int> { TileID.Stone, MiningSubworld.Slate }, Main.UnderworldLayer - 100, false, 100);
 
             UltimateSkyblock.Instance.Logger.Info("Generating Gold or Platinum");
-            LoopWorldAndGenerateTilesWithDepthModifiers(11, Main.rand.Next(4, 8), Main.rand.Next(5, 8), SubVars.gold, new List<int> { TileID.Stone, MiningSubworld.Slate }, Main.UnderworldLayer - 100, false, 100);
+            LoopWorldAndGenerateTilesWithDepthModifiers(8, Main.rand.Next(7, 10), Main.rand.Next(4, 6), SubVars.gold, new List<int> { TileID.Stone, MiningSubworld.Slate }, Main.UnderworldLayer - 100, false, 100);
         }
     }
 }

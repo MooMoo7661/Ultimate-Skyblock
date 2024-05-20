46b0230098c6aced75dd43dece13f6e5940f610c
Moo Moo <obsidian1392@gmail.com>
2024-05-20 12:59:37 -0600

    tile merge utils

---
+++ Utils/FramingUtils.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+
+namespace UltimateSkyblock.Utils
+{
+    public static class FramingUtils
+    {
+        public static void SetupTileMerge(this ModTile tile, int tile2)
+        {
+            Main.tileMerge[tile.Type][tile2] = true;
+            Main.tileMerge[tile2][tile.Type] = true;
+        }
+
+        public static void SetupTileMerge(this ModTile tile, ModTile tile2)
+        {
+            Main.tileMerge[tile.Type][tile2.Type] = true;
+            Main.tileMerge[tile2.Type][tile.Type] = true;
+        }
+
+        public static void SetupTileMerge(int tile1, int tile2)
+        {
+            Main.tileMerge[tile1][tile2] = true;
+            Main.tileMerge[tile2][tile1] = true;
+        }
+    }
+}

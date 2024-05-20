99e9c4824ee6b9d3185b54b6687d18cbc5028b92
Moo Moo <obsidian1392@gmail.com>
2024-05-20 12:56:43 -0600

    Added deepstone brick beam

---
+++ Content/Tiles/Blocks/DeepstoneBrickBeamTile.cs
@@ -0,0 +1,21 @@
+﻿using Terraria.ID;
+using Terraria;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using UltimateSkyblock.Content.Subworlds;
+using UltimateSkyblock.Content.Tiles.Environment.Foliage;
+using UltimateSkyblock.Content.Items.Placeable.Tiles;
+
+namespace UltimateSkyblock.Content.Tiles.Blocks
+{
+    public class DeepstoneBrickBeamTile : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            TileID.Sets.IsBeam[Type] = true;
+            
+            AddMapEntry(Color.SlateGray);
+            DustType = DustID.Stone;
+        }
+    }
+}

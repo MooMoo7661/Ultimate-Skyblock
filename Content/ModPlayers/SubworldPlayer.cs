4b8122c2fcb40799423a6bfd883d32c23715e0fe
Moo Moo <obsidian1392@gmail.com>
2024-05-20 12:22:45 -0600

    Changed subworld detection to use this mod's and not every subworld

--- Content/ModPlayers/SubworldPlayer.cs
+++ Content/ModPlayers/SubworldPlayer.cs
@@ -14,34 +14,14 @@ namespace UltimateSkyblock.Content.ModPlayers
 	{
         public override void PreUpdate()
         {
-            if (SubworldSystem.AnyActive())
+            if (SubworldSystem.IsActive<MiningSubworld>() || SubworldSystem.IsActive<DungeonSubworld>())
             {
                 Player.ZoneSkyHeight = false;
                 Player.ZoneBeach = false;
                 Main.oceanBG = 0;
             }            
-             
-            // Commented code below is flashlight functionality
-            //if (Player.HeldItem.type == ModContent.ItemType<Content.Items.Placeable.Glimmercap>() && !Framing.GetTileSafely(Main.MouseWorld.ToTileCoordinates().X, Main.MouseWorld.ToTileCoordinates().Y).HasTile)
-            //{
-            //    Lighting.AddLight(Main.MouseWorld, 6, 6, 6);
-            //}
-        }
-
-        //public override void PostUpdate()
-        //{
-        //    if (Player.HeldItem.type == ModContent.ItemType<Content.Items.Placeable.Glimmercap>() && !Framing.GetTileSafely(Main.MouseWorld.ToTileCoordinates().X, Main.MouseWorld.ToTileCoordinates().Y).HasTile)
-        //    {
-        //        Lighting.AddLight(Main.MouseWorld, 6, 6, 6);
-        //    }
-        //}
-
-        public override void OnEnterWorld()
-        {
-            if (SubworldSystem.AnyActive())
-            {
-                Main.NewText("This world is seperate from the main world.\nThis means bosses defeated in here will not sync with the main world, and bosses defeated in the main world will not sync in here.");
-            }
+                
+            SubworldSystem.hideUnderworld = SubworldSystem.Current == ModContent.GetInstance<MiningSubworld>() && ((Player.position.Y / 16) <= Main.UnderworldLayer - 20);
         }
     }
 }

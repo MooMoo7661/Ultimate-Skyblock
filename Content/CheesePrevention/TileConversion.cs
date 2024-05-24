8d350dd3880256bde06a9e9d227a4d86f8977ee7
Moo Moo <obsidian1392@gmail.com>
2024-05-24 11:42:18 -0600

    Moved hardmode ore cheese prevention to the new folder

---
+++ Content/CheesePrevention/TileConversion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UltimateSkyblock.Content.CheesePrevention
+{
+    public class TileConversion : GlobalTile
+    {
+        public override void KillTile(int i, int j, int type, ref bool fail, ref bool effectOnly, ref bool noItem)
+        {
+            if (HardmodeOres[type] && !Condition.Hardmode.IsMet() && UltimateSkyblock.IsSkyblock())
+                fail = true;
+        }
+
+        public static bool[] HardmodeOres = TileID.Sets.Factory.CreateBoolSet(
+            TileID.Cobalt,
+            TileID.Mythril,
+            TileID.Adamantite,
+            TileID.Palladium,
+            TileID.Orichalcum,
+            TileID.Titanium);
+    }
+}

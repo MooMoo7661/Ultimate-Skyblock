007afe9dadf07c42cfa7464c2bdb365f75dbcde9
Moo Moo <obsidian1392@gmail.com>
2024-05-24 11:42:27 -0600

    Added config for bugs

---
+++ Content/Configs/BugsConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader.Config;
+
+namespace UltimateSkyblock.Content.Configs
+{
+    public class BugsConfig : ModConfig
+    {
+        public override ConfigScope Mode => ConfigScope.ServerSide;
+
+        [BackgroundColor(172, 228, 170)]
+        [DefaultValue(false)]
+        public bool HeartCrystalDropFix { get; set; }
+    }
+}

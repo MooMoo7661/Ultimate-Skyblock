25018e11d774090b5fbdce2f13f8c38b3a647ec6
Moo Moo <obsidian1392@gmail.com>
2024-05-24 11:41:43 -0600

    Config option to fix potential heart crystals not dropping items

---
+++ Content/Bugfixes/GlobalTileDrops.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UltimateSkyblock.Content.Configs;
+using static Terraria.WorldGen;
+
+namespace UltimateSkyblock.Content.Bugfixes
+{
+    public class GlobalTileDrops : GlobalTile
+    {
+
+        public override void KillTile(int i, int j, int type, ref bool fail, ref bool effectOnly, ref bool noItem)
+        {
+            if (type == TileID.Heart && Main.tile[i, j].TileFrameX == 0 && Main.tile[i, j].TileFrameY == 0 && ModContent.GetInstance<BugsConfig>().HeartCrystalDropFix)
+            {
+                noItem = true;
+                Item.NewItem(GetItemSource_FromTileBreak(i, j), new Vector2(i * 16, j * 16), Vector2.Zero, ItemID.LifeCrystal);
+            }
+        }
+    }
+}

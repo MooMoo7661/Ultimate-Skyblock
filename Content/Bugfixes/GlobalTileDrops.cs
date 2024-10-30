f3d82b1f1076a997645d8d0c52ac20582ee4a4fa
Moo Moo <obsidian1392@gmail.com>
2024-10-30 12:00:07 -0600

    Yet more attempts at fixing heart crystal bug...

--- Content/Bugfixes/GlobalTileDrops.cs
+++ Content/Bugfixes/GlobalTileDrops.cs
@@ -1,22 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using UltimateSkyblock.Content.Configs;
-using static Terraria.WorldGen;
+﻿using static Terraria.WorldGen;
 
 namespace UltimateSkyblock.Content.Bugfixes
 {
     public class GlobalTileDrops : GlobalTile
     {
-
+        //Bandaid fix to some dumb fucking bug
         public override void KillTile(int i, int j, int type, ref bool fail, ref bool effectOnly, ref bool noItem)
         {
-            if (type == TileID.Heart && Main.tile[i, j].TileFrameX == 0 && Main.tile[i, j].TileFrameY == 0 && ModContent.GetInstance<BugsConfig>().HeartCrystalDropFix)
+            base.KillTile(i, j, type, ref fail, ref effectOnly, ref noItem);
+
+            if (type == TileID.Heart)
             {
+                if (Main.tile[i, j].TileFrameX == 0 && Main.tile[i, j].TileFrameY == 0)
+                {
+                    Item.NewItem(GetItemSource_FromTileBreak(i, j), new Vector2(i * 16, j * 16), Vector2.Zero, ItemID.LifeCrystal);
+                }
+
                 noItem = true;
-                Item.NewItem(GetItemSource_FromTileBreak(i, j), new Vector2(i * 16, j * 16), Vector2.Zero, ItemID.LifeCrystal);
             }
         }
     }

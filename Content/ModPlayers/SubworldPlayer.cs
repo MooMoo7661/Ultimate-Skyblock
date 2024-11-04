91dcd036017870ffc63cc6b1bbc8a037f75bd602
Moo Moo <obsidian1392@gmail.com>
2024-11-04 13:21:38 -0700

    Attempt at forcing dungeon zone

--- Content/ModPlayers/SubworldPlayer.cs
+++ Content/ModPlayers/SubworldPlayer.cs
@@ -7,6 +7,7 @@ using Terraria.ID;
 using Terraria.ModLoader;
 using Terraria.WorldBuilding;
 using Microsoft.Xna.Framework.Graphics;
+using Terraria.ObjectData;
 
 namespace UltimateSkyblock.Content.ModPlayers
 {
@@ -19,7 +20,13 @@ namespace UltimateSkyblock.Content.ModPlayers
                 Player.ZoneSkyHeight = false;
                 Player.ZoneBeach = false;
                 Main.oceanBG = 0;
-            }            
+            }
+
+            if (SubworldSystem.IsActive<DungeonSubworld>())
+            {
+                Player.ZoneDungeon = true;
+                Player.ZoneDirtLayerHeight = true;
+            }
                 
             SubworldSystem.hideUnderworld = SubworldSystem.Current == ModContent.GetInstance<MiningSubworld>() && ((Player.position.Y / 16) <= Main.UnderworldLayer - 20);
         }
